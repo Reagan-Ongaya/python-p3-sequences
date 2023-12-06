590e0efcdb1e953deb42aea30d06ab6285e75b3a
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-12-06 12:28:50 +0300

    Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    
+    if length >= 1:
+        fib_sequence.append(0)
+    if length >= 2:
+        fib_sequence.append(1)
+    
+    for i in range(2, length):
+        next_fib = fib_sequence[i-1] + fib_sequence[i-2]
+        fib_sequence.append(next_fib)
+    
+    print(fib_sequence)
+    
